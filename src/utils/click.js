a6575389c6b61ebba1ffe815f31679d73dfa43dd
hmm365 <happyham52@gmail.com>
2022-12-09 19:13:58 +0900

    1209

--- src/utils/click.js
+++ src/utils/click.js
@@ -7,22 +7,22 @@ function click() {
             // console.log(inx);
             switch (inx) {
                 case 0:
-                    document.querySelector('.headline').scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'nearest' })
+                    document.querySelector('.headline').scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' })
                     break
                 case 1:
-                    document.querySelector('.bulidUp').scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'nearest' })
+                    document.querySelector('.bulidUp').scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' })
                     break
                 case 2:
-                    document.querySelector('.skillUp').scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'nearest' })
+                    document.querySelector('.skillUp').scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' })
                     break
                 case 3:
-                    document.querySelector('.project').scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'nearest' })
+                    document.querySelector('.project').scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' })
                     break
                 case 4:
-                    document.querySelector('.studyWork').scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'nearest' })
+                    document.querySelector('.studyWork').scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' })
                     break
                 case 5:
-                    document.querySelector('.growing').scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'nearest' })
+                    document.querySelector('.growing').scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' })
                     break
                 default:
                     break
